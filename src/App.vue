cff2fd150bda8f8694aad30e468eebc36b45c0e8
Winston <wihardjawinston@gmail.com>
2026-01-24 13:44:21 +0700

    feat: Initialize main App component with basic Vue setup.

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup></script>
 
 <template>
-  <h1>hello welcome!</h1>
+  <h1>hello nama saya winston</h1>
   <p>
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
